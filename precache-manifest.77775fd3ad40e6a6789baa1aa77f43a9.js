8bf04a62c504ea8a82dc8163c20ddd66c39f7c15
Nathaniel Atkins <64879234+luximperium@users.noreply.github.com>
2020-09-28 13:28:50 -0400

    initial commit

---
+++ precache-manifest.77775fd3ad40e6a6789baa1aa77f43a9.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6177867810b76b8b6ba83d8c9d07e32c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d0bb6b3ad4d96597b21",
+    "url": "/static/css/2.a90685df.chunk.css"
+  },
+  {
+    "revision": "8ff1aae5205c9091d4c2",
+    "url": "/static/css/main.981466b6.chunk.css"
+  },
+  {
+    "revision": "6d0bb6b3ad4d96597b21",
+    "url": "/static/js/2.6a5d8a69.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.6a5d8a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ff1aae5205c9091d4c2",
+    "url": "/static/js/main.91fa5bd9.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "5bf43f4e8424624b645362c82190f0ee",
+    "url": "/static/media/AEP.5bf43f4e.jpg"
+  },
+  {
+    "revision": "df7a9813d620a24fe5c9db706985101d",
+    "url": "/static/media/AIMH.df7a9813.jpg"
+  },
+  {
+    "revision": "ef7b45f5f088fa4fce667db30793c186",
+    "url": "/static/media/ALC.ef7b45f5.jpg"
+  },
+  {
+    "revision": "a5909458d6b01082872bb01e818f242b",
+    "url": "/static/media/BLUE.a5909458.png"
+  },
+  {
+    "revision": "42e732f9b1e0dec4037159199e8e5eb4",
+    "url": "/static/media/Bastet.42e732f9.jpg"
+  },
+  {
+    "revision": "ce0de1fc25af495912b0db6bedaddfbb",
+    "url": "/static/media/BastetVIP.ce0de1fc.png"
+  },
+  {
+    "revision": "e0f231b2b532af13073300d2900d95c0",
+    "url": "/static/media/GOLD.e0f231b2.png"
+  },
+  {
+    "revision": "cef9a92812282f469dce8500a3adeafe",
+    "url": "/static/media/Jacob.cef9a928.jpg"
+  },
+  {
+    "revision": "61dee987492c4bdde9cabe32796ef78d",
+    "url": "/static/media/JacobPortfolio.61dee987.png"
+  },
+  {
+    "revision": "4c6f2caa779afddcad744eb8014541c2",
+    "url": "/static/media/Lux.4c6f2caa.png"
+  },
+  {
+    "revision": "8b51156ff3eb3948e958ed41052b4a5b",
+    "url": "/static/media/Lux3.8b51156f.png"
+  },
+  {
+    "revision": "c2b7c4f8b88d92481e074160b3185a8a",
+    "url": "/static/media/MusiqueBase.c2b7c4f8.png"
+  },
+  {
+    "revision": "8082b9375ec7c55c48004367e9fa2ebe",
+    "url": "/static/media/NA.8082b937.png"
+  },
+  {
+    "revision": "14049890133cd9e1a8e4d65f82bd474d",
+    "url": "/static/media/Nurom-Bold.14049890.woff2"
+  },
+  {
+    "revision": "7331f9719ed728ac2dbd9e9fcd8e539b",
+    "url": "/static/media/Nurom-Bold.7331f971.svg"
+  },
+  {
+    "revision": "77950ea8a841cd5e820187642ab7794f",
+    "url": "/static/media/Nurom-Bold.77950ea8.woff"
+  },
+  {
+    "revision": "d68e28910397742ed599f004b5dcdfea",
+    "url": "/static/media/Nurom-Bold.d68e2891.eot"
+  },
+  {
+    "revision": "db615590ba6b4f39f8e7c0400f6f615a",
+    "url": "/static/media/Nurom-Bold.db615590.ttf"
+  },
+  {
+    "revision": "e4e4a377b34f5f82b770eecf29a38c69",
+    "url": "/static/media/Nurom-Bold.e4e4a377.otf"
+  },
+  {
+    "revision": "331ed56961ac33cb1efab888032b21ea",
+    "url": "/static/media/OSRS.331ed569.png"
+  },
+  {
+    "revision": "fc2d44f179e4c97378e1412cbed878f2",
+    "url": "/static/media/POL.fc2d44f1.jpg"
+  },
+  {
+    "revision": "659e1d6acfa48493c71d7ab7fe6fc5d0",
+    "url": "/static/media/RED.659e1d6a.png"
+  },
+  {
+    "revision": "c2e3bface1e13a5ee5edeeb9dcefb96a",
+    "url": "/static/media/Resume.c2e3bfac.pdf"
+  },
+  {
+    "revision": "765077cc4e732f52fcf57bae3b56c1ac",
+    "url": "/static/media/STORM.765077cc.jpg"
+  },
+  {
+    "revision": "1e25413ebac146447010a2dc4c673e6b",
+    "url": "/static/media/Spray.1e25413e.png"
+  },
+  {
+    "revision": "7f4119f0a249de606354d177c501875a",
+    "url": "/static/media/SpraySoundCloud.7f4119f0.jpg"
+  },
+  {
+    "revision": "7ae102e9cfbb98593f68cb8090b93ec8",
+    "url": "/static/media/SynthTime1.7ae102e9.png"
+  },
+  {
+    "revision": "de957275ea9ef99ed266b6bdc39fc52d",
+    "url": "/static/media/SynthTime21.de957275.png"
+  },
+  {
+    "revision": "ebd6c9e8eacf1f52edb67015f64f45a2",
+    "url": "/static/media/SynthTime22.ebd6c9e8.png"
+  },
+  {
+    "revision": "c49bcc9b7e211f845add3c24abee0685",
+    "url": "/static/media/SynthTime3.c49bcc9b.png"
+  },
+  {
+    "revision": "316a1364c6fc0f498e114b8c1f98162f",
+    "url": "/static/media/TakeItBack.316a1364.png"
+  },
+  {
+    "revision": "43fe7ae9ae7bfd8ddbad268d786c5ca1",
+    "url": "/static/media/brackets.43fe7ae9.png"
+  },
+  {
+    "revision": "6a8abd2d9c535e4c47ba58b1f33bbec5",
+    "url": "/static/media/linkedin.6a8abd2d.png"
+  },
+  {
+    "revision": "048291cc2b8a20ec5445b5270624a7b4",
+    "url": "/static/media/netflixclone.048291cc.png"
+  },
+  {
+    "revision": "c762d850e2e8d0e29047608715196736",
+    "url": "/static/media/roboto-all-400-italic.c762d850.woff"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "8266321ab43353bcd147ad67600d165c",
+    "url": "/static/media/roboto-cyrillic-400-italic.8266321a.woff2"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "b3e580d221a4722c959155878ab94210",
+    "url": "/static/media/roboto-cyrillic-ext-400-italic.b3e580d2.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "469a78405c3ae073ba769321fa0584f3",
+    "url": "/static/media/roboto-greek-400-italic.469a7840.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "801b64f75ae26dab9c36f00629ebdad0",
+    "url": "/static/media/roboto-greek-ext-400-italic.801b64f7.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/static/media/roboto-latin-400-italic.51521a2a.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "dc3871f486e189164db4b98968330bc4",
+    "url": "/static/media/roboto-latin-ext-400-italic.dc3871f4.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "9780bde87bceec579eaf16bddad47615",
+    "url": "/static/media/roboto-vietnamese-400-italic.9780bde8.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  }
+]);
